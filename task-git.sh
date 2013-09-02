e9ec91594ad3c722be13ffb069184ad65626cdc5
Kosta Harlan <kostajh@gmail.com>
2013-09-02 16:54:02 -0400

    Remove task-git-push from arguments if set

--- task-git.sh
+++ task-git.sh
@@ -11,16 +11,24 @@ if [ ! -d "$DATA_DIR" ]; then
   exit 1
 fi
 
-# Create backups
+# Check if --task-git-push is passed as an argument.
+PUSH=0
+for i
+do
+  if [ "$i" == "--task-git-push" ]; then
+    # Set the PUSH flag, and remove this from the arguments list.
+    PUSH=1
+    shift
+  fi
+done
+
+# Call task, commit files and push if flag is set.
 /usr/bin/task $@
 cd $DATA_DIR
 git add .
 git commit -m "$TASK_COMMAND" > /dev/null
-# Check if --task-git-push is passed as an argument.
-for i
-do
-  if [ "$i" == "--task-git-push" ]; then
-    git push origin master
-  fi
-done
+
+if [ "$PUSH" == 1 ]; then
+  git push origin master > /dev/null
+fi
 exit 0
